14dde54bc763c3cd51158448a62756b99f6b7d09
dar-jeeling <o_o___t@pusan.ac.kr>
2023-06-13 12:29:09 +0900

    ✨ feat: main.ts에 controller 들 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ImageController } from './image/image.controller';
 import { ImageService } from './image/image.service';
-
+import { HttpModule } from '@nestjs/axios';
+import { OpenAiController } from './openai/openai.controller';
+import { Shop11stController } from './11st/11st.controller';
 @Module({
-  imports: [],
-  controllers: [ImageController],
+  imports: [HttpModule],
+  controllers: [ImageController, OpenAiController, Shop11stController],
   providers: [ImageService],
 })
 export class AppModule {}
