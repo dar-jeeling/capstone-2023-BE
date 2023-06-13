4ac18272e64373ad1248a6172e6f5b26a3f228f1
dar-jeeling <o_o___t@pusan.ac.kr>
2023-06-13 12:29:33 +0900

    ✨ feat: CORS 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,14 @@ config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // CORS 설정 추가
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
